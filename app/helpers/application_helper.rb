5486dd7473ae5019fa7276df2b8cbb41f6b8965d
Matias Korhonen <matias@kiskolabs.com>
2012-08-07 16:04:52 +0300

    Removes an extra helper method that slipped in.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,4 @@
 module ApplicationHelper
-  def link_to_add_fields(name, f, association)
-    new_object = f.object.send(association).klass.new
-    id = new_object.object_id
-    fields = f.fields_for(association, new_object, child_index: id) do |builder|
-      render(association.to_s.singularize + "_fields", f: builder)
-    end
-    link_to(name, '#', class: "btn add_fields", data: {id: id, fields: fields.gsub("\n", "")})
-  end
-
   def conditional_html( lang = "en", &block )
     haml_concat Haml::Util::html_safe <<-"HTML".gsub( /^\s+/, '' )
       <!--[if lt IE 7 ]>              <html lang="#{lang}" class="no-js ie6"> <![endif]-->
