75b3bb5249bd2eadb1082f4278571c26a4790760
Matias Korhonen <matias@kiskolabs.com>
2012-08-07 16:08:35 +0300

    Include the @-symbol in the username links. #2

--- app/helpers/statuses_helper.rb
+++ app/helpers/statuses_helper.rb
@@ -2,10 +2,11 @@ module StatusesHelper
   include Twitter::Autolink
 
   def format_status_text(status)
-    if status.is_retweet?
-      auto_link_with_json(status.raw_hash[:retweeted_status][:text], status.raw_hash[:retweeted_status][:entities]).html_safe
+    text, entities = if status.is_retweet?
+      [status.raw_hash[:retweeted_status][:text], status.raw_hash[:retweeted_status][:entities]]
     else
-      auto_link_with_json(status.text, status.raw_hash[:entities]).html_safe
+      [status.text, status.raw_hash[:entities]]
     end
+    auto_link_with_json(text, entities, { username_include_symbol: true }).html_safe
   end
 end
