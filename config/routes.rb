2fc254f52fb394dd6aea6af34269d15444de15a6
Matias Korhonen <matias@kiskolabs.com>
2012-09-15 14:03:52 +0300

    Restores the sessions route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ TweetDepository::Application.routes.draw do
   root to: "statuses#index"
   match "/search", to: "statuses#search", as: :search
   match "auth/:provider/callback", to: "sessions#create"
+  resource :sessions, only: [ :destroy ]
 end
