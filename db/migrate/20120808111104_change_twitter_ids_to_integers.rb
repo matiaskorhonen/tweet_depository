eb70e0adf9417ade3cf46ff6eca4d51e6dba1f56
Matias Korhonen <matias@kiskolabs.com>
2012-09-15 13:10:06 +0300

    Try to fix the migration issues on Postgres.

--- db/migrate/20120808111104_change_twitter_ids_to_integers.rb
+++ db/migrate/20120808111104_change_twitter_ids_to_integers.rb
@@ -1,6 +1,10 @@
 class ChangeTwitterIdsToIntegers < ActiveRecord::Migration
   def up
-    change_column :statuses, :twitter_id, :integer, limit: 8
+    if ActiveRecord::Base.connection.instance_of? ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
+      Status.connection.execute('ALTER TABLE statuses ALTER twitter_id TYPE BIGINT USING twitter_id::bigint;')
+    else
+      change_column :statuses, :twitter_id, :integer, limit: 8
+    end
   end
 
   def down
--- db/migrate/20120817145542_convert_user_uids_to_big_ints.rb
+++ db/migrate/20120817145542_convert_user_uids_to_big_ints.rb
@@ -1,6 +1,10 @@
 class ConvertUserUidsToBigInts < ActiveRecord::Migration
   def up
-    change_column :users, :uid, :integer, limit: 8
+    if ActiveRecord::Base.connection.instance_of? ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
+      User.connection.execute('ALTER TABLE users ALTER uid TYPE BIGINT USING uid::bigint;')
+    else
+      change_column :users, :uid, :integer, limit: 8
+    end
   end
 
   def down
