6e2411fc96cea02697a3e8a6ca3bc1577181f3d8
Matias Korhonen <matias@kiskolabs.com>
2012-10-03 23:52:06 +0300

    Minor fix to the format_status_text helper

--- app/helpers/statuses_helper.rb
+++ app/helpers/statuses_helper.rb
@@ -2,7 +2,7 @@ module StatusesHelper
   include Twitter::Autolink
 
   def format_status_text(status)
-    text, entities = if status.is_retweet?
+    text, entities = if status.is_retweet? && status.raw_hash[:retweeted_status]
       [status.raw_hash[:retweeted_status][:text], status.raw_hash[:retweeted_status][:entities]]
     else
       [status.text, status.raw_hash[:entities]]
